ed5382757da18115deb77d03224f245c539d9163
aidancz <aidanczhai@gmail.com>
2025-12-01 12:21:03 +0800

    simplify dot-repeat

--- lua/tT.lua
+++ lua/tT.lua
@@ -41,52 +41,88 @@ end
 
 -- # the following is for single-char dot-repeatable search
 
-M.search_literal_string_opts = function(opts)
-	M.search_literal_string(opts.count, opts.direction, opts.literal_string, opts.search_history)
-end
+M.cache_n = {
+-- normal mode (include visual mode)
+	count = 1,
+	direction = "next",
+	literal_string = " ",
+	search_history = true,
+}
 
-M.cache_opts_operator_pending_mode = nil
-M.cache_opts_operator_pending_mode_not = nil
+M.cache_o = {
+-- operator pending mode
+	count = 1,
+	direction = "next",
+	literal_string = " ",
+	search_history = false,
+}
 
-M.apply_cache_opts = function(is_operator_pending_mode)
-	local cache_opts
-	if is_operator_pending_mode then
-		cache_opts = M.cache_opts_operator_pending_mode
-	else
-		cache_opts = M.cache_opts_operator_pending_mode_not
-	end
+M.cache_n_apply = function()
 	if vim.v.count ~= 0 then
-		cache_opts.count = vim.v.count
+		M.cache_n.count = vim.v.count
 	end
-	M.search_literal_string_opts(cache_opts)
+	M.search_literal_string(
+		M.cache_n.count,
+		M.cache_n.direction,
+		M.cache_n.literal_string,
+		M.cache_n.search_history
+	)
 end
 
----@param opts {
+M.cache_o_apply = function()
+	if vim.v.count ~= 0 then
+		M.cache_o.count = vim.v.count
+	end
+	M.search_literal_string(
+		M.cache_o.count,
+		M.cache_o.direction,
+		M.cache_o.literal_string,
+		M.cache_o.search_history
+	)
+end
+
+---@param opts? {
 ---	count?: number,
----	direction: "prev"|"next",
+---	direction?: "prev"|"next",
 ---	literal_string?: string,
 ---	search_history?: boolean,
 ---}
 M.expr = function(opts)
-	opts.count = opts.count or vim.v.count1
-	opts.literal_string = opts.literal_string or vim.fn.getcharstr():gsub("\r", "\\n")
+	opts = opts or {}
+	if opts.literal_string == nil then
+		opts.literal_string = vim.fn.getcharstr():gsub("\r", "\\n")
+	end
 
-	local mode = vim.api.nvim_get_mode().mode
-	local is_operator_pending_mode = string.sub(mode, 1, 2) == "no"
-	if is_operator_pending_mode then
-		opts.search_history = false
-		M.cache_opts_operator_pending_mode = opts
-		return
-		[[<cmd>lua require("tT").apply_cache_opts(true)<cr>]]
+	if M.is_operator_pending_mode() then
+		M.cache_o = vim.tbl_extend(
+			"force",
+			{
+				count = 1,
+				direction = "next",
+				literal_string = " ",
+				search_history = false,
+			},
+			opts
+		)
+		return [[<cmd>lua require("tT").cache_o_apply()<cr>]]
 	else
-		opts.search_history = true
-		M.cache_opts_operator_pending_mode_not = opts
-		return
-		[[<cmd>lua require("tT").apply_cache_opts(false)<cr>]]
+		M.cache_n = vim.tbl_extend(
+			"force",
+			{
+				count = 1,
+				direction = "next",
+				literal_string = " ",
+				search_history = true,
+			},
+			opts
+		)
+		return [[<cmd>lua require("tT").cache_n_apply()<cr>]]
 	end
 end
--- seperate M.expr?
--- yes, you can seperate them:
--- but nobody wants to map a same key twice
+
+M.is_operator_pending_mode = function()
+	local mode = vim.api.nvim_get_mode().mode
+	return string.sub(mode, 1, 2) == "no"
+end
 
 return M
